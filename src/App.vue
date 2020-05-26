5b9ce573d90abe434645904245fd1a3955fa2f8f
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2020-05-27 01:50:04 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<home></home>
+</template>
+
+<script>
+import home from './components/home.vue';
+export default {
+  name: 'App',
+  components: {
+    home
+  },
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap');
+
+*{
+  padding: 0;
+  margin: 0;
+  font-family: 'Anonymous Pro', monospace;
+}
+body{
+  overflow: hidden;
+}
+</style>
